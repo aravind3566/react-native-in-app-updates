83ee60afd559996a2cb41c951040b09e07ce9678
Aravind <aravind3566@gmail.com>
2026-06-12 08:40:03 +0700

    chore: untrack generated lib/ directory

--- lib/typescript/module/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum UpdateFlow {
-    IMMEDIATE = "IMMEDIATE",
-    FLEXIBLE = "FLEXIBLE"
-}
-export declare function checkForUpdate(updateFlow: UpdateFlow): any;
-//# sourceMappingURL=index.d.ts.map
